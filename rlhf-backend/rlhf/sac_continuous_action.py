96cd13e1b269871e3111eba8005f46235693c87b
TonqiS <priv.thanglongnguyen@gmail.com>
2024-12-09 11:44:09 +0100

    Video fixed, Used experimental wrapper instead of default, defined training_period to define which nth episode to create a video

--- rlhf-backend/rlhf/sac_continuous_action.py
+++ rlhf-backend/rlhf/sac_continuous_action.py
@@ -35,7 +35,7 @@ class Args:
     """whether to capture videos of the agent performances (check out `videos` folder)"""
 
     # Algorithm specific arguments
-    env_id: str = "Humanoid-v4"
+    env_id: str = "Hopper-v4"
     """the environment id of the task"""
     total_timesteps: int = 1000000
     """total timesteps of the experiments"""
@@ -68,7 +68,7 @@ def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
         if capture_video and idx == 0:
             env = gym.make(env_id, render_mode="rgb_array")
-            env = gym.experimental.wrappers.RecordVideoV0(env, f"videos/{run_name}", name_prefix="eval", episode_trigger=lambda x: x % training_period == 0)
+            env = gym.experimental.wrappers.RecordVideoV0(env, f"videos/{run_name}", name_prefix="training", episode_trigger=lambda x: x % training_period == 0)
         else:
             env = gym.make(env_id)
         env = gym.wrappers.RecordEpisodeStatistics(env)
