b8cffd80b0966d174292f80cdcd9e798f4c8d6ac
Malik Durmus <malikdurmus2@gmail.com>
2024-12-16 21:08:54 +0100

    Refactor preference predictor to improve method encapsulation
    replaced `numpy` operations with `torch` equivalents for consistency. Adjusted `train_reward_model` to operate as a class method and removed redundant parameters.

--- rlhf-backend/rlhf/preference_predictor.py
+++ rlhf-backend/rlhf/preference_predictor.py
@@ -9,7 +9,7 @@ class PreferencePredictor:
         self.reward_network = reward_network
 
     # for each data in sample we will use the predict function. after that we will use the preference to calculate entropy loss
-    def compute_predicted_probability(self, trajectories):
+    def _compute_predicted_probability(self, trajectories):
         """
         :param trajectories: A tuple consisting of two trajectories, where each trajectory is an iterable of tuples. Each tuple represents (action, state) pairs from the trajectory.
         :return: A scalar value representing the predicted probability that the human will choose the first trajectory over the second. Returns None if the trajectories have mismatched lengths.
@@ -33,10 +33,10 @@ class PreferencePredictor:
                                                   observation=torch.tensor(state))  # estimated probability, that the human will prefer action 1
             total_prob1 += prob_for_action
 
-        predicted_prob = np.exp(total_prob0) / (np.exp(total_prob0) + np.exp(total_prob1)) #probability, that the human will chose trajectory0 over trajectory1
+        predicted_prob = torch.exp(total_prob0) / (torch.exp(total_prob0) + torch.exp(total_prob1)) #probability, that the human will chose trajectory0 over trajectory1
         return predicted_prob
 
-    def compute_loss(self, sample):
+    def _compute_loss(self, sample):
         """
         :param sample: A list of tuples where each tuple contains a pair of trajectories and their corresponding human feedback label.
                        The human feedback label is an indicator of which trajectory is preferred by the human.
@@ -45,7 +45,7 @@ class PreferencePredictor:
         """
         entropy_loss = 0
         for trajectory_pair, human_feedback_label in sample:
-            predicted_prob = self.compute_predicted_probability(trajectory_pair)
+            predicted_prob = self._compute_predicted_probability(trajectory_pair)
             # human feedback label to tensor conversion for processing
             label_1 = torch.tensor(human_feedback_label, dtype=torch.float)
             label_2 = 1 - label_1
@@ -59,25 +59,24 @@ class PreferencePredictor:
 
         return entropy_loss #loss for whole batch
 
-    def train_reward_model(reward_network, sample, lr, epochs):
+    def train_reward_model(self, sample, lr, epochs=1): #remove later
         # Recap: Function compute_predicted_probability, gives us a scalar value for the probability that the human chooses trajectory 0 over trajectory 1
         # Compute_loss takes the human_feedback_label and calculates the entropy loss
         # This function aims to change the weights of the reward model, so that it minimizes the entropy loss
         """
-        :param reward_network: The reward model which weights are updated to minimize the entropy loss calculated with compute_loss
         :param sample: A list of tuples where each tuple contains a pair of trajectories and their corresponding human feedback label.
         :param lr: learning rate
         :param epochs: training steps
         :return:
         """
-        optimizer = optim.Adam(reward_network.parameters(), lr=lr)  # Optimizer used for reward model: Adam
+        optimizer = optim.Adam(list(self.reward_network.parameters()), lr=lr)  # Optimizer used for reward model: Adam
 
         for epoch in range(epochs):
             # Reset Gradients
             optimizer.zero_grad()
 
             # Calculate entropy loss
-            entropy_loss = reward_network.compute_loss(sample)
+            entropy_loss = self._compute_loss(sample)
 
             # Backpropagation
             entropy_loss.backward()
