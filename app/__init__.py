b58d55f770cbd036d5735f3a776ddb4c8e6e6655
Tobias Huber <huberto@cip.ifi.lmu.de>
2025-01-23 02:36:05 +0100

    fixed videos, added autoplay

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,9 @@ from app.utils import BASE_DIR, TEMPLATES_DIR, STATIC_DIR, video_queue, preferen
 
 def create_app(run_name, preference_buffer):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key_here' # TODO: remove later
     CORS(app)
     socketio = SocketIO(app, cors_allowed_origins="*")
-    VIDEO_DIR = os.path.join(f"videos/{run_name}")
-    os.makedirs(VIDEO_DIR, exist_ok=True)
+    os.makedirs(os.path.join(f"videos/{run_name}"), exist_ok=True)
 
     @app.route("/", methods=["GET"])
     def serve_landing_page():
@@ -24,26 +22,31 @@ def create_app(run_name, preference_buffer):
             video_pairs.append({'id': pair_id, 'video1': video1_path, 'video2': video2_path})
         return jsonify({'video_pairs': video_pairs})
 
-    @app.route('/videos/<run_name>/<filename>')
-    def serve_video(run_name, filename):
-        return send_from_directory(VIDEO_DIR, filename)
+    @app.route("/get_run_name", methods=["GET"])
+    def get_run_name():
+        return jsonify({'global_run_name': run_name})
+
+    @app.route('/videos/<flask_run_name>/<filename>')
+    def serve_video(flask_run_name, filename):
+        video_dir = os.path.join("../videos", flask_run_name)   # TODO make this an absolute path eventually
+        return send_from_directory(video_dir, filename)
 
     @app.route('/submit_preferences', methods=['POST'])
     def submit_preferences():
         feedback = request.json.get('preferences', [])
-        print("feedback is",feedback)
+        print("Received feedback: ",feedback)
         for feedback_item in feedback:
             pair_id = feedback_item['id']
             preference = feedback_item['preference']
             with preference_mutex:
                 trajectory_data = next((item for item in stored_pairs if item['id'] == pair_id), None)
                 if trajectory_data:
-                    preference_buffer.add( (trajectory_data['trajectory1'], trajectory_data['trajectory2']), preference)
+                    preference_buffer.add((trajectory_data['trajectory1'], trajectory_data['trajectory2']), preference)
 
-        feedback_event.set() # Signal training thread
+        feedback_event.set()  # Signal training thread
         return jsonify({'status': 'success'})
 
     def notify_new_video_pairs():
-        # this notifies the frontend that the videos are ready, (no need for inefficient polling)
         socketio.emit('new_video_pairs', {'status': 'ready'})
+
     return app, socketio, notify_new_video_pairs, video_queue, stored_pairs, feedback_event, preference_mutex
